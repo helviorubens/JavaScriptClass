5e2f5fd4272563f11b808d30898507b52ac40632
Helvio R. Albuquerque <helvio.albuquerque@gmail.com>
2023-03-22 21:07:51 -0300

    Adicionado exemplos com array e função

--- destructuring.js
+++ destructuring.js
@@ -23,3 +23,21 @@ console.log(sobrenome, bemHumorada)
 
 const {endereco: {logradouro, numero, cep}} = pessoa
 console.log(logradouro, numero, cep)
+
+// Operador de desestruturação para array
+
+let [a, , c, d = -1] = [1, 2, 3]
+console.log(a, c, d)
+
+
+// Operador de desestruturação em função
+
+function rand({ min = 0, max = 1000}){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+const obj = { max: 40, min: 10}
+console.log(rand(obj))
+console.log(rand({ min: 95})) // o valor máximo é definido como 1000 por padrão
+console.log(rand({})) // parâmetros vazios
