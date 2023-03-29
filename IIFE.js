fc63483a9e62aacb3ada8b15ce2d314b8624ca7b
Hélvio Albuquerque <helvio.albuquerque@gmail.com>
2023-03-29 16:08:38 -0300

    Add novo exemplo mais intuitivo

--- IIFE.js
+++ IIFE.js
@@ -2,11 +2,12 @@
  * IIFE (Immediately Invoked Function Expression)
  * Funções Imediatas
  * 
- * An IIFE is a JavaScript function that runs as soon as it is defined.
+ * An IIFE ou expressão de função imediatamente invocada é chamada assim pois é criada como uma expressão e executada imediatamente. Ver exemplo em "conceitos gerais.js"
  */
 
 // AVOID POLLUTING THE GLOBAL NAMESPACE
 
+/*
 let adder = (function() {
     let myPhrase = ""
     return function(x) { 
@@ -17,4 +18,26 @@ let adder = (function() {
     
    console.log(adder("Olá"))  // "Olá"
    console.log(adder("Mundo!")) // "Olá Mundo!"
-  
+   */
+
+/**
+ * https://ricardo-reis.medium.com/express%C3%A3o-de-fun%C3%A7%C3%A3o-imediatamente-invocada-b706a7ee02f4
+ */
+
+  const calculator = (function() {
+    function add(a, b){
+        return a + b
+    }
+
+    function multiply(a, b){
+        return a * b
+    }
+
+    return {
+        add: add,
+        multiply: multiply
+    }
+  })()
+
+  console.log(calculator.add(10, 20))
+  console.log(calculator.multiply(10, 20))
