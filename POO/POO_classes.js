a15396a6f720900715af4a961330b784bcd38007
Hélvio Albuquerque <helvio.albuquerque@gmail.com>
2023-12-26 22:00:58 -0300

    Exemplo com método usado para validar propriedade

--- POO/POO_classes.js
+++ POO/POO_classes.js
@@ -1,1 +1,32 @@
-// exemplo
+class Person {
+    constructor(name) {
+        this._name = this._capitalize(name)
+    }
+
+    set name(newName) {
+        this._name = this._capitalize(newName)
+    }
+
+    get name() {
+        return this._name
+    }
+
+    /** Método responsável por "validar" a informação contida na propriedade "name"
+     * 
+     * @param {String} text 
+     * @returns 
+     */
+    _capitalize(text) {
+        return `${text.toUpperCase()}`
+    }
+
+    walk() {
+        return `${this._name} is walking...`
+    }
+}
+
+
+let helvio = new Person('Hélvio')
+console.log(helvio.name)
+let rubens = new Person('Rubens')
+console.log(rubens.name)
