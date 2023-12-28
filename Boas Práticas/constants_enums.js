e04d5b862aca865f068212b2b4951805c28388bc
Hélvio Albuquerque <helvio.albuquerque@gmail.com>
2023-12-28 07:43:16 -0300

    Usando constants e enum-like par aorganizar código

---
+++ Boas Práticas/constants_enums.js
@@ -0,0 +1,19 @@
+/** @module Employee */
+
+/** Constants and Enums */
+
+const LIST_API_URL = "employee/"
+
+
+/**
+ * @enum
+ * Posible address types.
+ */
+let AddressType = {
+    /** Signifies the address type us a house. */
+    House: "House",
+    /** Signifies the address type us an apartment. */
+    Apartment: "Apartment",
+    /** Signifies the address type us a suite. */
+    Suite: "Suite"
+}
