f6bdbdf075e749f261594b161bc61e63bb9e209c
Hélvio Albuquerque <helvio.albuquerque@gmail.com>
2024-10-19 05:58:19 -0300

    Modificação da descrição

--- Herança/herança sem protótipo.js
+++ Herança/herança sem protótipo.js
@@ -2,7 +2,9 @@
  * HERANÇA SEM PROTÓTIPO
  * Criar um objeto completamente vazio, sem o protótipo padrão, como `toString()` ou `hasOwnProperty()`.
  * 
- * Útil para criação de tabelas hash simples ou dicionários.
+ * Útil para:
+ *    - criação de tabelas hash simples ou dicionários;
+ *    - evitar colisões de propriedades com o protótipo padrão. * 
  */
 
 const dictionary = Object.create(null);
